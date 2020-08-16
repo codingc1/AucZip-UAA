d5a7cc04cbf861e559828a70d3e8fb499e222e50
조규창 <codingc@hanmail.net>
2020-08-16 23:03:45 +0900

    client build

---
+++ server/public/precache-manifest.e4039415c7737a96b976ab9b3bffb5c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f3cfcbc122a4b4f9cff5027e41824e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65b630f6d9a79cb90cab",
+    "url": "/static/css/main.efbc4e8e.chunk.css"
+  },
+  {
+    "revision": "e4564567614b68233ca2",
+    "url": "/static/js/2.5ef681d8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5ef681d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b630f6d9a79cb90cab",
+    "url": "/static/js/main.3fb3ae97.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "b22d95de0e50294f96e3ae80bd92904f",
+    "url": "/static/media/AucZip_title_fullsize.b22d95de.png"
+  },
+  {
+    "revision": "39c6088edd6a679cf84af7a9d9440cfb",
+    "url": "/static/media/DoHyeon-Regular.39c6088e.ttf"
+  }
+]);
